43f5614dce8d059ca887193b53e62b4ce51c84f5
Hsury <i@hsury.com>
2017-12-08 21:53:16 +0800

    摩擦轮发射程序包提供反馈真实速度的接口

--- rc_python/src/ejector.py
+++ rc_python/src/ejector.py
@@ -4,14 +4,14 @@ class Ejector():
     '''UESTC 2018 Robocon Team
     Ejector Package
     '''
-    def __init__(self, leftPort='COM3', rightPort='COM5', baudrate=19200):
+    def __init__(self, leftPort='COM3', rightPort='COM6', baudrate=19200):
         self._port = [leftPort, rightPort]
         self._baudrate = baudrate
         self._elmoList = [Elmo(self._port[0], self._baudrate), \
                           Elmo(self._port[1], self._baudrate)]
         self._v = 0
     
-    def go(self, v=100000):
+    def go(self, v=200000):
         self._v = v
         self._elmoList[0].speed = self._v
         self._elmoList[1].speed = - self._v
@@ -24,6 +24,11 @@ class Ejector():
     def stop(self):
         self.go(v=0)
     
+    def release(self):
+        self.stop()
+        self._elmoList[0].disable()
+        self._elmoList[1].disable()
+    
     @property
     def speed(self):
-        return self._v
+        return [self._elmoList[0].speed, self._elmoList[1].speed]
