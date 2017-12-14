3e07f4d7c52cda2200a676feed635481cd3f415c
Hsury <i@hsury.com>
2017-12-14 19:41:10 +0800

    修复流程中的坐标参数

--- rc_python/src/flow.py
+++ rc_python/src/flow.py
@@ -25,9 +25,9 @@ class Flow():
                 'id': 3,
                 'func': self.__TZA2TZ1
             },
-            'Wait For Ball': {
+            'Wait For Ball At TZ1': {
                 'id': 4,
-                'func': self.__waitForBall
+                'func': self.__waitAtTZ1
             },
             'From TZ1 To TZA': {
                 'id': 5,
@@ -41,9 +41,21 @@ class Flow():
                 'id': 7,
                 'func': self.__TZB2TZ2
             },
-            'From TZ2 To TZ3': {
+            'Wait For Ball At TZ2': {
                 'id': 8,
+                'func': self.__waitAtTZ2
+            },
+            'From TZ2 To TZ3': {
+                'id': 9,
                 'func': self.__TZ22TZ3
+            },
+            'Wait For Ball At TZ3': {
+                'id': 10,
+                'func': self.__waitAtTZ3
+            },
+            'From TZ3 To ARSZ': {
+                'id': 11,
+                'func': self.__TZ32ARSZ
             }
         }
         self._dash = dash
@@ -84,13 +96,13 @@ class Flow():
 
     # ID 1, Param Init
     def __paramInit(self):
-        self._dash._merge.swift(0.6, 7.5)
+        self._dash._merge.swift(0.55, 7.54)
         print('Swift to: {}'.format(self._dash.position))
         return 'From ARSZ To TZA'
 
     # ID 2, From ARSZ To TZA
     def __ARSZ2TZA(self):
-        self._dash.to(2, 3, 0)
+        self._dash.to(1.625, 3.055, 0)
         self._dash.unlock()
         while self._dash.resDist >= 0.2:
             sleep(0.005)
@@ -99,14 +111,14 @@ class Flow():
 
     # ID 3, From TZA To TZ1
     def __TZA2TZ1(self):
-        self._dash.to(3.8, 3, 0)
+        self._dash.to(3.775, 3.055, 0)
         while self._dash.resDist >= 0.05:
             sleep(0.005)
         print('Arrive with resultant distance: {}'.format(self._dash.resDist))
-        return 'Wait For Ball'
+        return 'Wait For Ball At TZ1'
 
-    # ID 4, Wait For Ball
-    def __waitForBall(self):
+    # ID 4, Wait For Ball At TZ1
+    def __waitAtTZ1(self):
         print('Wait for 2 second')
         sleep(2)
         print('Done')
@@ -114,7 +126,7 @@ class Flow():
 
     # ID 5, From TZ1 To TZA
     def __TZ12TZA(self):
-        self._dash.to(2, 3, 0)
+        self._dash.to(1.625, 3.055, 0)
         while self._dash.resDist >= 0.2:
             sleep(0.005)
         print('Arrive with resultant distance: {}'.format(self._dash.resDist))
@@ -122,7 +134,7 @@ class Flow():
 
     # ID 6, From TZA To TZB
     def __TZA2TZB(self):
-        self._dash.to(2, 1, 0)
+        self._dash.to(1.625, 1.035, 0)
         while self._dash.resDist >= 0.2:
             sleep(0.005)
         print('Arrive with resultant distance: {}'.format(self._dash.resDist))
@@ -130,16 +142,42 @@ class Flow():
 
     # ID 7, From TZB To TZ2
     def __TZB2TZ2(self):
-        self._dash.to(3.8, 1, 0)
+        self._dash.to(3.775, 1.035, 0)
         while self._dash.resDist >= 0.05:
             sleep(0.005)
         print('Arrive with resultant distance: {}'.format(self._dash.resDist))
+        return 'Wait For Ball At TZ2'
+
+    # ID 8, Wait For Ball At TZ2
+    def __waitAtTZ2(self):
+        print('Wait for 2 second')
+        sleep(2)
+        print('Done')
         return 'From TZ2 To TZ3'
 
-    # ID 8, From TZ2 To TZ3
+    # ID 9, From TZ2 To TZ3
     def __TZ22TZ3(self):
-        self._dash.to(7, 1, 0)
+        self._dash.to(7.035, 1.035, 0)
         while self._dash.resDist >= 0.05:
             sleep(0.005)
+        print('Arrive with resultant distance: {}'.format(self._dash.resDist))
+        return 'Wait For Ball At TZ3'
+
+    # ID 10, Wait For Ball At TZ3
+    def __waitAtTZ3(self):
+        print('Wait for 2 second')
+        sleep(2)
+        print('Done')
+        return 'From TZ3 To ARSZ'
+
+    # ID 11, From TZ3 To ARSZ
+    def __TZ32ARSZ(self):
+        self._dash.to(1.625, 1.035, 0)
+        while self._dash.resDist >= 0.2:
+            sleep(0.005)
+        self._dash.to(0.55, 7.54, 0)
+        while self._dash.resDist >= 0.05:
+            sleep(0.005)
+        self._dash.lock()
         print('Arrive with resultant distance: {}'.format(self._dash.resDist))
         return None
